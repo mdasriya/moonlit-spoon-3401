416e05c8ef6a2c6b8776e29d617d03c301969ed9
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2023-05-04 12:23:01 +0530

    projectdata

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <div className="App">
-     
+     <Footer />
     </div>
   );
 }
