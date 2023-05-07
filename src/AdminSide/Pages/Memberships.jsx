19b8f9bc0b56b842a5c43495c9d4690f05bf0f9a
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-05-07 14:27:04 +0530

    day5

--- src/AdminSide/Pages/Memberships.jsx
+++ src/AdminSide/Pages/Memberships.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavAdmin from '../components/NavAdmin'
 import { getMemberships } from '../Redux-Admin/action'
 import { useEffect,useState } from 'react'
+import MemberCard from '../components/MemberCard'
+import styled from 'styled-components'
 
 const Memberships = () => {
  const [member,setMember]= useState([]);
@@ -19,9 +21,25 @@ const Memberships = () => {
    
     <div>
          <NavAdmin/>
-         {"map here"}
+       <DIV>
+
+       {member.length>0 && member.map((item)=>{
+            return  <MemberCard key={item.id} {...item}/>   
+         })}
+       </DIV>
+        
     </div>
   )
 }
 
 export default Memberships
+
+
+
+let DIV = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    margin-top: 10px;
+    gap: 20px;
+    
+`
