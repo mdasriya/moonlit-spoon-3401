1c5afa11389921f81136d346a3a51d0b172ec228
Gauravshukla82 <gauravshukla.ec1022@gmail.com>
2023-05-08 16:20:07 +0530

    day-5

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,44 +1,87 @@
-import { Box, Image, Button } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Image, Button } from "@chakra-ui/react";
+import React from "react";
 
-import { HashLink as Link } from 'react-router-hash-link'
+import { HashLink as Link } from "react-router-hash-link";
 const Navbar = () => {
-  const myStyle ={
-    color:"#F1F4FC",
-    textDecoration:"none"
-  }
+  const myStyle = {
+    color: "#F1F4FC",
+    textDecoration: "none",
+  };
   return (
-
-    <Box style={{ position:"fixed",zIndex:"100", display: "flex", justifyContent: "space-around", width: "100%", alignItems: "center", textDecoration: "none",top:"0",backgroundColor:"#06181C"}}>
-
+    <Box
+      style={{
+        position: "fixed",
+        zIndex: "100",
+        display: "flex",
+        justifyContent: "space-around",
+        width: "100%",
+        alignItems: "center",
+        textDecoration: "none",
+        top: "0",
+        backgroundColor: "#06181C",
+      }}
+    >
       <Box style={{ width: "10%" }}>
         <Link to="/#" smooth>
-          <Image src='https://i.ibb.co/vPM31c0/Logo.png' width="100px">
-          </Image>
+          <Image src="https://i.ibb.co/vPM31c0/Logo.png" width="100px"></Image>
         </Link>
-
       </Box>
-      <div style={{ width: "40%", justifyContent: "space-around", display: "flex"}}>
-
-       
-        <Link style={myStyle} to="/#coaches" smooth>COACHES</Link>
-        <Link style={myStyle} to="/blog">BLOG</Link>
-        <Link style={myStyle} to="/#about-us" smooth>ABOUT US</Link>
-        <Link style={myStyle} to="/#contact-us" smooth>CONTACT US</Link>
-
+      <div
+        style={{
+          width: "40%",
+          justifyContent: "space-around",
+          display: "flex",
+        }}
+      >
+        <Link style={myStyle} to="/#coaches" smooth>
+          COACHES
+        </Link>
+        <Link style={myStyle} to="/blog">
+          BLOG
+        </Link>
+        <Link style={myStyle} to="/#about-us" smooth>
+          ABOUT US
+        </Link>
+        <Link style={myStyle} to="/#contact-us" smooth>
+          CONTACT US
+        </Link>
       </div>
 
-      <Box style={{width:"15%",justifyContent:"space-between",display:"flex"}}>
+      <Box
+        style={{
+          width: "15%",
+          justifyContent: "space-between",
+          display: "flex",
+        }}
+      >
         <Link to={"/products"}>
-        <Button borderRadius="2px" backgroundColor="#2FD0DA" border="none" padding="5px" color="#06181C" fontWeight="bold">SHOP</Button>
-
+          <Button
+            borderRadius="2px"
+            backgroundColor="#2FD0DA"
+            border="none"
+            padding="5px"
+            color="#06181C"
+            fontWeight="bold"
+          >
+            SHOP
+          </Button>
         </Link>
         <Link to="/member">
-          <Button cursor="pointer" borderRadius="2px" backgroundColor="#2FD0DA" border="none" padding="5px" color="#06181C" fontWeight="bold">BECOME A MEMBER</Button>
+          <Button
+            cursor="pointer"
+            borderRadius="2px"
+            backgroundColor="#2FD0DA"
+            border="none"
+            padding="5px"
+            color="#06181C"
+            fontWeight="bold"
+          >
+            BECOME A MEMBER
+          </Button>
         </Link>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
