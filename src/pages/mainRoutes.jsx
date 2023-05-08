9d40ced354055d47d3b1dbcdce28a866b051e85d
Gauravshukla82 <gauravshukla.ec1022@gmail.com>
2023-05-08 17:36:51 +0530

    signup

--- src/pages/mainRoutes.jsx
+++ src/pages/mainRoutes.jsx
@@ -1,61 +1,61 @@
-import React from 'react'
+import React from "react";
 
 import Dashboard from "../AdminSide/Pages/Dashboard";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AllProducts from "../AdminSide/Pages/AllProducts";
-import AddProducts from '../AdminSide/Pages/AddProducts';
-import AboutUs from './AboutUs'
-import Blog from './Blog'
-import Classes from './Classes'
-import Coaches from './Coaches'
-import Home from './Home'
+import AddProducts from "../AdminSide/Pages/AddProducts";
+import AboutUs from "./AboutUs";
+import Blog from "./Blog";
+import Classes from "./Classes";
+import Coaches from "./Coaches";
+import Home from "./Home";
 
-import SingleProducts from './SingleProducts'
-import Product from './Product'
+import SingleProducts from "./SingleProducts";
+import Product from "./Product";
 
-import Member from './Member';
-import Contact from './Contact';
+import Member from "./Member";
+import Contact from "./Contact";
 
-import Login from './Login';
-import SignUp from './SignUp';
+import Login from "./Login";
+import SignUp from "./SignUp";
 
-import Memberships from '../AdminSide/Pages/Memberships';
+import Memberships from "../AdminSide/Pages/Memberships";
 import Users from "../AdminSide/Pages/Users";
-import Cart from './Cart';
-
-
-
+import Cart from "./Cart";
+import PrivateRoute from "../Components/PrivateRoute";
 
 const MainRoutes = () => {
   return (
-
     <Routes>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/allproducts" element={<AllProducts/>}/>
-      <Route path='/addProducts' element={<AddProducts/>}/>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/memberships' element={<Memberships/>}/>
-      <Route path='/users' element={<Users/>}/>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/allproducts" element={<AllProducts />} />
+      <Route path="/addProducts" element={<AddProducts />} />
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/memberships" element={<Memberships />} />
+      <Route path="/users" element={<Users />} />
 
-        <Route path='/classes' element={<Classes/>}></Route>
-        <Route path='/coaches' element={<Coaches/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<SignUp/>}></Route>
-        <Route path='/blog' element={<Blog/>}></Route>
-        <Route path='/about-us' element={<AboutUs/>}></Route>
+      <Route path="/classes" element={<Classes />}></Route>
+      <Route path="/coaches" element={<Coaches />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/blog" element={<Blog />}></Route>
+      <Route path="/about-us" element={<AboutUs />}></Route>
 
-        <Route path='/products' element={<Product/>}></Route>
-        <Route path='/products/:id' element={<SingleProducts/>}></Route>
+      <Route path="/products" element={<Product />}></Route>
+      <Route path="/products/:id" element={<SingleProducts />}></Route>
 
-        <Route path='/member' element={<Member/>}></Route>
-        <Route path='/contact-us' element={<Contact/>}></Route>
-        <Route path='/cart' element={<Cart/>}></Route>
-      
+      <Route
+        path="/member"
+        element={
+          <PrivateRoute>
+            <Member />
+          </PrivateRoute>
+        }
+      ></Route>
+      <Route path="/contact-us" element={<Contact />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+    </Routes>
+  );
+};
 
-      </Routes>
-
-
-  )
-}
-
-export default MainRoutes
+export default MainRoutes;
--- src/redux/authReducer/action.js
+++ src/redux/authReducer/action.js
@@ -1,4 +1,4 @@
-import { LOGIN_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS } from "./actionType";
+import { LOGIN_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS, POST_USER_ERROR, POST_USER_REQUEST, POST_USER_SUCCESS } from "./actionType";
 import axios from "axios";
 
 export const login = (anyNameObj) => (dispatch) => {
@@ -11,3 +11,20 @@ export const login = (anyNameObj) => (dispatch) => {
     })
     .catch((err) => dispatch({ type: LOGIN_ERROR }));
 };
+
+
+export const signUp = (newUser) => (dispatch) => {
+  dispatch({ type: POST_USER_REQUEST });
+  return axios
+    .post(`https://powerful-blue-smock.cyclic.app/users`, newUser)
+    .then((res) => {
+      console.log(res.data);
+      dispatch({ type: POST_USER_SUCCESS });
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch({ type: POST_USER_ERROR });
+    });
+};
+
+
